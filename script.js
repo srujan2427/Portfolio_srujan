810521797f0539166f6c5c256465e1a5f41c3f3f
srujan2427 <srujanchowdary1219@gmail.com>
2026-08-13 13:19:18 +0530

    day9changes

--- script.js
+++ script.js
@@ -23,4 +23,44 @@ console.log(heroTitle);
 if(heroTitle){
     heroTitle.textContent = `${getGreeting()}, I'm P.Srujan 👋`;
 }
+ // ==== MOBILE MENU TOGGLE ===
+ 
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+ 
+menuToggle.addEventListener('click', () =>{
+    navLinks.classList.toggle('open');
+    menuToggle.setAttribute('aria-expanded',navLinks.classList.contains('open'));
+});
+ 
+// === SCROLL-BASED NAVBAR STYLING ====
+const header = document.querySelector('.site-header');
+ 
+window.addEventListener('scroll', () =>{
+    if (window.scrollY > 50){
+        header.classList.add('scrolled')
+    }
+    else{
+        header.classList.remove('scrolled')
+    }
+})
+ 
+// === ACTIVE NAV LINK on scroll ====
+const sections = document.querySelectorAll('section[id]');
+const navItems = document.querySelectorAll('.nav-links a');
+ 
+window.addEventListener('scroll', () =>{
+    let current ='';
+    sections.forEach(section =>{
+        if(window.scrollY >= section.offsetTop-100){
+            current=section.getAttribute('id');
+        }
+    })
+    navItems.forEach(link =>{
+        link.classList.remove('active');
+        if(link.getAttribute('href') === `#${current}`) {
+            link.classList.add('active');
+        }
+    })
+})
  
