74c5c87ef081fc99ae30b372761f6ea0e62db807
srujan2427 <srujanchowdary1219@gmail.com>
2026-08-14 13:11:24 +0530

    day10_changes

--- script.js
+++ script.js
@@ -63,4 +63,45 @@ window.addEventListener('scroll', () =>{
         }
     })
 })
+
+ //Day 10 - Project Filter System
+const projects =[
+    { id: 1, name: "Weather App", category: "web", tech: ["React", "API"] },
+    { id: 2, name: "Todo App", category: "web", tech: ["JavaScript"] },
+    { id: 3, name: "Portfolio", category: "design", tech: ["HTML", "CSS"] },
+    { id: 4, name: "Calculator", category: "web", tech: ["JavaScript"] },
+ 
+];
+ 
+function renderProjects(filter="all") {
+    const grid = document.querySelector('.projects-grid');
+    const filtered = filter === "all"
+    ? projects
+    : projects.filter(p => p.category === filter);
+ 
+    grid.innerHTML = filtered.map(project =>
+        `<article class="project-card">
+        <div class="project-card-body">
+        <h3>${project.name}</h3>
+        <div class="project-tags">
+        ${project.tech.map(t => `<span class="tag">${t}</span>`).join('')}      
+        </div>
+        <a href="#" class=" btn btn-primary">View Project</a>
+        </div>
+        </article>
+        `).join('');
+}
+ 
+//Filter buttons
+document.querySelectorAll('.filter-btn').forEach(btn =>{
+    btn.addEventListener('click',()=>{
+        document.querySelectorAll('.filter-btn').forEach(b =>
+            b.classList.remove('active'));
+            btn.classList.add('active')
+            renderProjects(btn.dataset.filter);
+});
+    });
+ 
+//Intial render
+renderProjects();
  
